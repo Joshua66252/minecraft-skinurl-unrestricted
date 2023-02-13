c3d6eb79c2fcd48aec5b48797c46c166e25a5322
Joshua66252 <44351387+Joshua66252@users.noreply.github.com>
2023-02-13 17:20:43 -0600

    Change to 1.19.2 + Specifics
    Changed to be 1.19.2 instead of 1.19.3
    Also changed to not be example mod id and things

--- src/main/java/net/dbit/skinurlunrestrict/mixin/Mixin.java
+++ src/main/java/net/dbit/skinurlunrestrict/mixin/Mixin.java
@@ -1,16 +1,15 @@
-package net.fabricmc.example.mixin;
+package net.dbit.skinurlunrestrict.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.dbit.skinurlunrestrict.Main;
 import net.minecraft.client.gui.screen.TitleScreen;
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
+@org.spongepowered.asm.mixin.Mixin(TitleScreen.class)
+public class Mixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Main.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
