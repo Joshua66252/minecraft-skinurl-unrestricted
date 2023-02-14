6f9a38398c883e3327278473314d993facbd282a
Joshua66252 <44351387+Joshua66252@users.noreply.github.com>
2023-02-13 21:23:29 -0600

    add html

---
+++ minecraft-headmaster/script.js
@@ -0,0 +1,34 @@
+var hName = document.getElementById("hName")
+var hUrl = document.getElementById("hUrl")
+var output = document.getElementById("output")
+buttonClick = function(){
+	let hNameVal = hName.value
+	let hUrlVal = hUrl.value
+	let strBuild = ""
+	let b64Build = ""
+	let strPt1 = '/give @p player_head{SkullOwner:{Id:[I;0,0,0,0],Name:"'
+	let strPt2 = '",Properties:{textures:[{Value:"'
+	let strPt3 = '"}]}}}'
+	let b64Pt1 = '{"textures":{"SKIN":{"url":"'
+	let b64Pt2 = '"}}}'
+	strBuild+=strPt1
+	strBuild+=hNameVal
+	strBuild+=strPt2
+	b64Build+=b64Pt1
+	b64Build+=hUrlVal
+	b64Build+=b64Pt2
+	strBuild+=btoa(b64Build)
+	strBuild+=strPt3
+	output.innerHTML = strBuild
+	//B64
+}
+clipBoard = function(){
+	var copyText = output.innerHTML
+	if(copyText != ""){
+	    navigator.clipboard.writeText(copyText).then(() => {
+	        alert("Copied to clipboard")
+	    })
+	}else{
+		alert("Nothing to copy")
+	}
+}
