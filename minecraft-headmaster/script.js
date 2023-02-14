194ca731f5f2228883b3ab44c2eddc0623f46542
Joshua66252 <44351387+Joshua66252@users.noreply.github.com>
2023-02-14 02:05:29 -0600

    Update website

--- minecraft-headmaster/script.js
+++ minecraft-headmaster/script.js
@@ -1,13 +1,16 @@
 var hName = document.getElementById("hName")
 var hUrl = document.getElementById("hUrl")
+var hFull = document.getElementById("hFull")
 var output = document.getElementById("output")
 buttonClick = function(){
+	let hFullVal = hFull.checked
 	let hNameVal = hName.value
 	let hUrlVal = hUrl.value
 	let strBuild = ""
 	let b64Build = ""
 	let strPt1 = '/give @p player_head{SkullOwner:{Id:[I;0,0,0,0],Name:"'
 	let strPt2 = '",Properties:{textures:[{Value:"'
+	let hFullstrPt3 = '"}]}},Tags:["fullbody"]}'
 	let strPt3 = '"}]}}}'
 	let b64Pt1 = '{"textures":{"SKIN":{"url":"'
 	let b64Pt2 = '"}}}'
@@ -18,7 +21,11 @@ buttonClick = function(){
 	b64Build+=hUrlVal
 	b64Build+=b64Pt2
 	strBuild+=btoa(b64Build)
-	strBuild+=strPt3
+	if(hFullVal){
+		strBuild+=hFullstrPt3
+	}else{
+		strBuild+=strPt3
+	}
 	output.innerHTML = strBuild
 	//B64
 }
